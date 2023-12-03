c3027ed267c5d7bf933acf1baaac16cceddadf2f
NoMoreActimel <evolution3000futurama@gmail.com>
2023-12-04 00:15:21 +0300

    swapped test.py and inference.py

--- test.py
+++ test.py
@@ -1,71 +1,157 @@
+import argparse
 import json
-import torch
-import numpy as np
-
+import os
 from pathlib import Path
 
-from utils import get_WaveGlow
-import hw_nv.waveglow as waveglow
+import torch
+from tqdm import tqdm
+
+import hw_nv.loss as module_loss
+import hw_tts.model as module_model
+from hw_tts.utils import ROOT_PATH
+from hw_tts.utils.object_loading import get_dataloaders
+from hw_tts.utils.parse_config import ConfigParser
+from inference import run_inference
+
+DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "default_test_model" / "checkpoint.pth"
 
 
-def move_batch_to_device(batch, device: torch.device):
-    """
-    Move all necessary tensors to the HPU
-    """
-    for tensor_for_gpu in ["mel", "wav"]:
-        batch[tensor_for_gpu] = batch[tensor_for_gpu].to(device)
+def main(config):
+    logger = config.get_logger("test")
+
+    # define cpu or gpu if possible
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # setup data_loader instances
+    dataloaders = get_dataloaders(config)
+
+    # build model architecture
+    model = module_model.HiFiGANModel(config.config)
+    logger.info(model)
+
+    logger.info("Loading checkpoint: {} ...".format(config.resume))
+    checkpoint = torch.load(config.resume, map_location=device)
+    state_dict = checkpoint["state_dict"]
+    if config["n_gpu"] > 1:
+        model = torch.nn.DataParallel(model)
+    model.load_state_dict(state_dict)
+
+    # prepare model for testing
+    model = model.to(device)
+
+    criterion = {
+        "generator": config.init_obj(
+            config["loss"]["generator"],
+            module_loss,
+            mel_generator=dataloaders["train"].dataset.melspec_generator
+        ).to(device),
+        "discriminator": config.init_obj(
+            config["loss"]["discriminator"],
+            module_loss
+        ).to(device)
+    }
     
-    return batch
-
-
-def run_inference(
-        model,
-        dataset,
-        indices,
-        inference_path,
-        compute_losses=False,
-        output_loss_filepath=None,
-        criterion=None,
-        dataset_type="train",
+    run_inference(
+        model=model,
+        dataset=dataloaders["val"].dataset,
+        indices=[30, 40, 50],
+        dataset_type="val",
+        inference_path="data/datasets/ljspeech/inference",
+        compute_losses=True,
+        output_loss_filepath="output.json",
+        criterion=criterion,
         epoch=None
-    ):
-    inference_path = Path(inference_path) / dataset_type
-    inference_path.mkdir(exist_ok=True, parents=True)
-    inference_paths = [inference_path / f"utterance_{ind}" for ind in indices]
+    )
     
-    for i, path in enumerate(inference_paths):
-        if epoch is not None:
-            inference_paths[i] = path / f"epoch_{epoch}"
-        inference_paths[i].mkdir(exist_ok=True, parents=True)
-    
-    if compute_losses:
-        output_loss_filepath = Path(output_loss_filepath) / dataset_type
-        output_loss_filepath.mkdir(exist_ok=True, parents=True)
+    print(f"Generation results written into data/datasets/ljspeech/inference/val folder")
+    print(f"Losses in output.json")
 
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(description="PyTorch Template")
+    args.add_argument(
+        "-c",
+        "--config",
+        default=None,
+        type=str,
+        help="config file path (default: None)",
+    )
+    args.add_argument(
+        "-r",
+        "--resume",
+        default=str(DEFAULT_CHECKPOINT_PATH.absolute().resolve()),
+        type=str,
+        help="path to latest checkpoint (default: None)",
+    )
+    args.add_argument(
+        "-d",
+        "--device",
+        default=None,
+        type=str,
+        help="indices of GPUs to enable (default: all)",
+    )
+    args.add_argument(
+        "-t",
+        "--test-data-folder",
+        default=None,
+        type=str,
+        help="Path to dataset",
+    )
+    args.add_argument(
+        "-b",
+        "--batch-size",
+        default=20,
+        type=int,
+        help="Test dataset batch size",
+    )
+    args.add_argument(
+        "-j",
+        "--jobs",
+        default=1,
+        type=int,
+        help="Number of workers for test dataloader",
+    )
 
-    dataset_items = [dataset[ind] for ind in indices]
-    batch = dataset.collate_fn(dataset_items)
-    batch = move_batch_to_device(batch, device='cuda:0')
+    args = args.parse_args()
 
-    with torch.no_grad():
-        batch["wav_gen"] = model(**batch)
-        batch["D_outputs"] = model.discriminate(wav=batch["wav"], wav_gen=batch["wav_gen"])
-        
-        if compute_losses:
-            discriminator_losses = criterion["discriminator"](**batch)
-            discriminator_loss_names = "discriminator_loss", "MPD_loss", "MSD_loss"
-            for i, loss_name in enumerate(discriminator_loss_names):
-                batch[loss_name] = discriminator_losses[i]
+    # set GPUs
+    if args.device is not None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = args.device
 
-            generator_losses = criterion["generator"](**batch)
-            generator_loss_names = "generator_loss", "GAN_loss", "mel_loss", "fm_loss"
-            for i, loss_name in enumerate(generator_loss_names):
-                batch[loss_name] = generator_losses[i]
-            
-            with open(output_loss_filepath, 'w', encoding='utf-8') as f:
-                json.dump(output_loss_filepath, f, ensure_ascii=False, indent=4)    
+    # first, we need to obtain config with model parameters
+    # we assume it is located with checkpoint in the same folder
+    model_config = Path(args.resume).parent / "config.json"
+    with model_config.open() as f:
+        config = ConfigParser(json.load(f), resume=args.resume)
 
-    for i, (ind, wav, wav_gen) in enumerate(zip(indices, batch["wav"], batch["wav_gen"])):
-        np.save(inference_paths[i] / f"wav_gen_{ind}.wav", wav_gen.cpu())
-        np.save(inference_paths[i] / f"wav_{ind}.wav", wav.cpu())
-        print(f"Saved utterance {ind} wav and wav_gen to {inference_paths[i]}")
+    # update with addition configs from `args.config` if provided
+    if args.config is not None:
+        with Path(args.config).open() as f:
+            config.config.update(json.load(f))
+
+    # if `--test-data-folder` was provided, set it as a default test set
+    if args.test_data_folder is not None:
+        test_data_folder = Path(args.test_data_folder).absolute().resolve()
+        assert test_data_folder.exists()
+        config.config["data"] = {
+            "val": {
+                "batch_size": args.batch_size,
+                "num_workers": args.jobs,
+                "datasets": [
+                    {
+                        "type": "CustomDirAudioDataset",
+                        "args": {
+                            "audio_dir": str(test_data_folder / "audio"),
+                            "transcription_dir": str(
+                                test_data_folder / "transcriptions"
+                            ),
+                        },
+                    }
+                ],
+            }
+        }
+
+    assert config.config.get("data", {}).get("val", None) is not None
+    config["data"]["val"]["batch_size"] = args.batch_size
+    config["data"]["val"]["n_jobs"] = args.jobs
+
+    main(config)
